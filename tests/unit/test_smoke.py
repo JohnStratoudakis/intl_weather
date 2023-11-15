e27cdde964d2ef538885b2a5749e835e783b777c
John Stratoudakis <john@Lenovo5>
2023-11-14 19:15:16 -0500

    Updating

--- tests/unit/test_smoke.py
+++ tests/unit/test_smoke.py
@@ -1,16 +1,30 @@
 
+#from weather.util import Util
+#   ut = Util()
+from weather.weather import Weather
+import tests.data
+from importlib.resources import files
 
-# Install intl_weather package
+# import Weather Install intl_weather package
 # Import Weather class
 #  - Call get zone
 #  - Call get hourly
 #  - Return current Temp + location + Image
+LAT_FLUSHING = "40.7607"
+LON_FLUSHING = "-73.7873"
 
-def test_one():
+#cache_dir = 'tests/data'
+def test_get_zone():
     # Types of queries:
     #  - Zone
     #  - Forecast
     #  - Hourly
     #  - Grid
-    pass
-
+    # GIVEN
+    lat = LAT_FLUSHING
+    lon = LON_FLUSHING
+    print(f'{tests.data}')
+    print(f'{files(tests.data)}')
+    # WHEN
+    _weather = Weather(lat=lat, lon=lon, cache_dir=cache_dir)
+    _weather.load_metadata(use_cache=True)
--- weather/weather.py
+++ weather/weather.py
@@ -1,23 +1,37 @@
 
+import json
+from pathlib import Path
+from typing import Optional
+
+
+class Metadata:
+    pass
 
 class Weather:
-    def __init__(self, lat: float, long: float):
+    def __init__(self, lat: float, lon: float,
+            cache_dir: Optional[Path] = None):
         self._lat = lat
-        self._long = long
+        self._lon = lon
+        if cache_dir is None:
+            cache_dir = Path('.')
+        else:
+            cache_dir = cache_dir
+
+        self._metadata = Metadata()
 
     def get_c_from_f(fahrenheit: float) -> float:
         return (fahrenheit - 32) * 5/9
 
-    def get_zone(self, record: bool=False, use_cache: bool=False) -> str:
+    def load_metadata(self, record: bool=False, use_cache: bool=False) -> str:
         """
         Find the gridX and gridY values for the given latitude
         and longitude pair, optionally save the response
         """
-        url = f'https://api.weather.gov/points/{self._lat},{self._long}'
+        url = f'https://api.weather.gov/points/{self._lat},{self._lon}'
 
         found_in_cache = False
         if use_cache:
-            data = Path(f'./cache/zone_{self._lat}_{self._long}.json').read_text()
+            data = Path(f'./cache/zone_{self._lat}_{self._lon}.json').read_text()
             data = json.loads(data)
             found_in_cache = True
         if use_cache is False and found_in_cache is False:
@@ -30,8 +44,16 @@ class Weather:
             if record:
                 if Path('./cache').exists() is False:
                     Path('./cache').mkdir()
-                Path(f'./cache/zone_{self._lat}_{self._long}.json').write_text(data_tab)
+                Path(f'./cache/zone_{self._lat}_{self._lon}.json').write_text(data_tab)
 
+        self._metadata.city = data['properties']['relativeLocation']['properties']['city']
+        self._metadata.state = data['properties']['relativeLocation']['properties']['state']
+        self._metadata.forecast_url = data['properties']['forecast']
+        self._metadata.forecast_hourly = data['properties']['forecastHourly']
+        self._metadata.forecast_griddata = data['properties']['forecastGridData']
+        self._metadata.forecast_stations = data['properties']['observationStations']
+        self._metadata.grid_x = data['properties']['gridX']
+        self._metadata.grid_y = data['properties']['gridY']
         return data
 
     def get_hourly(self,
