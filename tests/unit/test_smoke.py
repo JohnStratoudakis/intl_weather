41b0aa1c255464c7fd6f2f831dc7a29b5e4e6c9f
John Stratoudakis <john@Lenovo5>
2024-04-05 17:59:20 -0400

    Fixing tests

--- tests/unit/test_smoke.py
+++ tests/unit/test_smoke.py
@@ -1,7 +1,8 @@
-
-#from weather.util import Util
-#   ut = Util()
+from hamcrest import assert_that, equal_to
 from pathlib import Path
+import pytest
+
+from datetime import datetime, timedelta, timezone
 from weather.weather import Weather
 import tests.data
 from importlib.resources import files
@@ -24,9 +25,33 @@ def test_get_zone():
     # GIVEN
     lat = LAT_FLUSHING
     lon = LON_FLUSHING
-    print(f'{tests.data}')
-    print(f'{files(tests.data)}')
     cache_dir = Path(files(tests.data))
+
     # WHEN
     _weather = Weather(lat=lat, lon=lon, cache_dir=cache_dir)
-    _weather.load_metadata(use_cache=True)
+
+    # THEN
+    hourly = _weather.Hourly()
+    t = datetime(2024, 4, 5, 12, 56, 5, 0, tzinfo=timezone.utc)
+    assert_that(hourly.generatedAt(), equal_to(t))
+    assert_that(hourly.get_num_periods(), equal_to(156))
+    assert_that(hourly.get_period(idx=0).startTime(), equal_to(datetime(2024, 4, 5, 8, 0, 0, tzinfo=timezone(timedelta(hours=-4)))))
+    assert_that(hourly.get_period(idx=0).endTime(), equal_to(datetime(2024, 4, 5, 9, 0, 0, tzinfo=timezone(timedelta(hours=-4)))))
+
+    # Other fields of interest
+    # "isDaytime": true,
+    # "temperature": 41,
+    # "temperatureUnit": "F",
+
+    # "probabilityOfPrecipitation": {
+    # "unitCode": "wmoUnit:percent",
+    #    "value": 4
+    #},
+    #"relativeHumidity": {
+    #    "unitCode": "wmoUnit:percent",
+    #    "value": 70
+    #},
+    #"windSpeed": "14 mph",
+    #"windDirection": "W",
+    #"icon": "https://api.weather.gov/icons/land/day/bkn,4?size=small",
+    #"shortForecast": "Partly Sunny",
--- weather/weather.py
+++ weather/weather.py
@@ -1,54 +1,111 @@
 
+from datetime import datetime, timedelta, timezone
 import json
+import logging
 from pathlib import Path
+import requests
 from typing import Optional
 
+
+logger = logging.getLogger()
+
+
+date_fmt = '%Y-%m-%dT%H:%M:%S%z'
 
 class Metadata:
     pass
 
+class Period:
+    def __init__(self, in_json):
+        self._in_json = in_json
+
+    def startTime(self) -> datetime:
+        date_str = self._in_json['startTime']
+        return datetime.strptime(date_str, date_fmt)
+
+    def endTime(self) -> datetime:
+        date_str = self._in_json['endTime']
+        return datetime.strptime(date_str, date_fmt)
+
+class Hourly:
+    def __init__(self, in_json):
+        self._in_json = in_json
+
+    def generatedAt(self) -> datetime:
+        #"generatedAt": "2024-04-05T12:56:05+00:00",
+        date_str = self._in_json['properties']['generatedAt']
+        date_str = date_str[:-3] + date_str[-2:]
+
+        gen_date = datetime.strptime(date_str, date_fmt)
+        return gen_date
+
+    def get_period(self, idx) -> Period:
+        return Period(self._in_json['properties']['periods'][idx])
+
+    def get_num_periods(self) -> int:
+        return len(self._in_json['properties']['periods'])
 class Weather:
-    def __init__(self, lat: float, lon: float,
-            cache_dir: Optional[Path] = None):
+    def __init__(self,
+                 lat: float,
+                 lon: float,
+                 cache_dir: Optional[Path] = None,
+                 use_cache: bool = False):
         self._lat = lat
         self._lon = lon
         if cache_dir is None:
             self._cache_dir = Path('.')
         else:
             self._cache_dir = cache_dir
-        print(f'self._cache_dir: {self._cache_dir}')
+        #print(f'self._cache_dir: {self._cache_dir}')
 
         self._metadata = Metadata()
+        self._hourly = None
+        self._hourly_data = None
+        self._load_metadata(record=True, use_cache=use_cache)
+        self._load_hourly(record=True)
 
-    def get_c_from_f(fahrenheit: float) -> float:
+    def _make_request_with_cache(self,
+                                 url: str,
+                                 cache_file: Path,
+                                 record: bool = False) -> str:
+        result_str = None
+        print(f'Checking for cache file:\n  - {cache_file}')
+        if cache_file.exists() is False:
+            print(f'Cache file not found, making request')
+
+            r = requests.get(url=url)
+            # extracting data in json format
+            data = r.json()
+            if record:
+                result_str = json.dumps(data, indent=4)
+                if self._cache_dir.exists() is False:
+                    self._cache_dir.mkdir()
+                cache_file.write_text(result_str)
+        else:
+            print(f'Cache file found')
+            result_str = cache_file.read_text()
+            data = json.loads(result_str)
+
+        return data
+
+    def _get_c_from_f(fahrenheit: float) -> float:
         return (fahrenheit - 32) * 5/9
 
-    def load_metadata(self, record: bool=False, use_cache: bool=False) -> str:
+    def _load_metadata(self,
+                       record: bool=False,
+                       use_cache: bool=False) -> str:
         """
         Find the gridX and gridY values for the given latitude
         and longitude pair, optionally save the response
         """
         url = f'https://api.weather.gov/points/{self._lat},{self._lon}'
 
-        zone_json_file = self._cache_dir.joinpath(f'/zone_{self._lat}_{self._lon}.json')
-        if zone_json_file.exists():
+        zone_json_file = self._cache_dir.joinpath(f'zone_{self._lat}_{self._lon}.json')
+        print(f'zone_json_file: {zone_json_file}')
 
-        found_in_cache = False
-        if use_cache:
-            data = .read_text()
-            data = json.loads(data)
-            found_in_cache = True
-        if use_cache is False and found_in_cache is False:
-            r = requests.get(url=url)
-
-            # extracting data in json format
-            data = r.json()
-            data_tab = json.dumps(data, indent=4)
-
-            if record:
-                if self._cache_dir.exists() is False:
-                    self._cache_dir.mkdir()
-                self._cache_dir.joinpath(f'/zone_{self._lat}_{self._lon}.json').write_text(data_tab)
+        data = self._make_request_with_cache(url=url,
+                                             cache_file=zone_json_file,
+                                             record=record)
 
         self._metadata.city = data['properties']['relativeLocation']['properties']['city']
         self._metadata.state = data['properties']['relativeLocation']['properties']['state']
@@ -58,23 +115,23 @@ class Weather:
         self._metadata.forecast_stations = data['properties']['observationStations']
         self._metadata.grid_x = data['properties']['gridX']
         self._metadata.grid_y = data['properties']['gridY']
+
         return data
 
-    def get_hourly(self,
-                   forecast_hourly: str,
-                   grid_x: str,
-                   grid_y: str,
-                   record: bool=False,
-                   use_cache: bool=False) -> str:
-        r = requests.get(url=forecast_hourly)
+    def _load_hourly(self,
+                     record: bool=False,
+                     use_cache: bool=False) -> str:
 
-        #if use_cache:
-        #    data = Path(f'./cache/hourly_{
-        # extracting data in json format
-        data = r.json()
-        data_tab = json.dumps(data, indent=4)
-        if record:
-            Path(f'./cache/hourly_{grid_x}_{grid_y}.json').write_text(data_tab)
+        forecast_hourly_url = self._metadata.forecast_hourly
+        grid_x = self._metadata.grid_x
+        grid_y = self._metadata.grid_y
+        hourly_cache_file = self._cache_dir.joinpath(f'hourly_{grid_x}_{grid_y}.json')
 
-        return data_tab
+        data = self._make_request_with_cache(url=forecast_hourly_url,
+                                             cache_file=hourly_cache_file,
+                                             record=record)
+        self._hourly_data = data
+        self._hourly = Hourly(self._hourly_data)
 
+    def Hourly(self) -> Hourly:
+        return self._hourly
